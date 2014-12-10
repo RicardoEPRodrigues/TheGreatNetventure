9a6371fb3ff927719446797a690013433d57efa2
iandf93 <ian_tsamuxa@hotmail.com>
2014-12-10 17:27:46 +0000

    level 1 changed
    made a crazy coreography of virus

--- js/Levels/Level1.js
+++ js/Levels/Level1.js
@@ -1,6 +1,7 @@
 Scene.Level1 = function (game) {
     "use strict";
     Scene.Level0.call(this, game);
+    this.minTimeToWin = 24;
 };
 
 Scene.Level1.prototype = Object.create(Scene.Level0.prototype);
@@ -13,18 +14,51 @@ Scene.Level1.prototype.updateMalwares = function () {
 
     //        console.log(this.counter);
 
-    if (this.counter === 1) {
+    if (this.counter === 1 || this.counter === 15) {
         for (k = 0; k < 3; k = k + 1) {
-            alien = this.gameObjGenerator.getBasicVirus(k * 48 * 3 + 50, k * 50 + 50);
+            alien = this.gameObjGenerator.getBasicVirus(k * 100 + 150, 0);
             this.aliens.add(alien);
 
-            tween = this.game.add.tween(alien).to({ x: alien.position.x + 100 }, 2000, Phaser.Easing.Linear.None)
-            .to({ y: alien.position.y + 300 }, 1000, Phaser.Easing.Linear.None)
-            .to({ x: alien.position.x }, 2000, Phaser.Easing.Linear.None)
-            .to({ y: alien.position.y + 100 }, 1000, Phaser.Easing.Linear.None)
-            .start();
+			tween = this.game.add.tween(alien)
+            .to({ y: alien.position.y + 100}, 1000, Phaser.Easing.Linear.None)
+            .to({ y: alien.position.y + 100}, 2000, Phaser.Easing.Linear.None)
+			.to({ x: alien.position.x + 100, y: alien.position.y + 200 }, 1000, Phaser.Easing.Linear.None)
+            .to({ x: alien.position.x - 100, y: alien.position.y + 300 }, 1000, Phaser.Easing.Linear.None)
+			.to({ x: alien.position.x, y: alien.position.y + 400 }, 1000, Phaser.Easing.Linear.None)
+			.to({ y: alien.position.y + 200 }, 2000, Phaser.Easing.Linear.None)
+			.to({ y: alien.position.y + 200}, 2000, Phaser.Easing.Linear.None)
+			.to({ x: alien.position.x + 100, y: alien.position.y + 300 }, 1000, Phaser.Easing.Linear.None)
+            .to({ x: alien.position.x - 100, y: alien.position.y + 400 }, 1000, Phaser.Easing.Linear.None)
+			.to({ x: alien.position.x, y: alien.position.y + 500 }, 1000, Phaser.Easing.Linear.None)
+			.to({ x: alien.position.x, y: alien.position.y + 300 }, 2000, Phaser.Easing.Linear.None)
+            .to({ x: alien.position.x - 450, y: alien.position.y + 300 }, 1000, Phaser.Easing.Linear.None)
+			.start();
         }
     }
+    
+    if (this.counter === 8 || this.counter === 22) {
+        for (k = 0; k < 3; k = k + 1) {
+            alien = this.gameObjGenerator.getBasicVirus(k * 100 + 150, 0);
+            this.aliens.add(alien);
+
+			tween = this.game.add.tween(alien)
+            .to({ y: alien.position.y + 100}, 1000, Phaser.Easing.Linear.None)
+            .to({ y: alien.position.y + 100}, 2000, Phaser.Easing.Linear.None)
+			.to({ x: alien.position.x - 100, y: alien.position.y + 200 }, 1000, Phaser.Easing.Linear.None)
+            .to({ x: alien.position.x + 100, y: alien.position.y + 300 }, 1000, Phaser.Easing.Linear.None)
+			.to({ x: alien.position.x, y: alien.position.y + 400 }, 1000, Phaser.Easing.Linear.None)
+			.to({ y: alien.position.y + 200 }, 2000, Phaser.Easing.Linear.None)
+			.to({ y: alien.position.y + 200}, 2000, Phaser.Easing.Linear.None)
+			.to({ x: alien.position.x - 100, y: alien.position.y + 300 }, 1000, Phaser.Easing.Linear.None)
+            .to({ x: alien.position.x + 100, y: alien.position.y + 400 }, 1000, Phaser.Easing.Linear.None)
+			.to({ x: alien.position.x, y: alien.position.y + 500 }, 1000, Phaser.Easing.Linear.None)
+			.to({ x: alien.position.x, y: alien.position.y + 300 }, 2000, Phaser.Easing.Linear.None)
+            .to({ x: alien.position.x + 450, y: alien.position.y + 300 }, 1000, Phaser.Easing.Linear.None)
+			.start();
+        }
+    }
+	
+	
 
     this.counter = this.counter + 1;
 };
